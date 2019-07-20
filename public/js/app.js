24bcfc547632d188b02d2ee9b75cf08ad239c071
curoles <curoles@yahoo.com>
2019-07-19 22:32:44 -0700

    angular http

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,11 @@ if ("serviceWorker" in navigator) {
   console.log("Service worker is enabled in Navigator");
 }
 
+var app = angular.module('OneiroApp', []);
+
+app.controller('OneiroCtrl', function($scope, $http) {
+  $http.get("/api/atoms")
+  .then(function(response) {
+    $scope.oneiroAtoms = response.data;
+  });
+});

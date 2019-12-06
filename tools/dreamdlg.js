d3dad31fac256b1b776b41e3a7c9b6dff46ecbc9
curoles <curoles@yahoo.com>
2019-12-06 00:30:59 -0800

    dreamdlg: make infinite chat loop

--- tools/dreamdlg.js
+++ tools/dreamdlg.js
@@ -12,8 +12,6 @@
  */
 
 const program = require('commander');
-// Require logic.js file and extract controller functions using JS destructuring assignment
-//const { addContact, getContact } = require('./logic');
 
 program
     .version('0.0.1')
@@ -35,11 +33,53 @@ program
 
 program.parse(process.argv);
 
+// http://sudheerjonna.com/blog/2017/10/22/interactive-command-line-interface-using-inquirerjs/
+/** @see http://janabeck.com/blog/2017/02/05/infinite-interactivity-with-Inquirer */
+const inquirer = require('inquirer');
+const Rx = require('rxjs');
+
+const prompts = new Rx.Subject();
+
 /**
- * testing JSDoc
- * @param {string} id File identifier.
- * @returns {File} File object.
+ * Make `inquirer` prompt structure.
+ *
+ * @param {string} msg File identifier.
+ * @returns {any} prompt object.
  */
-const testJSDoc = (id) => {
-    // ...
+function makePrompt(msg) {
+    return {
+        type: 'input',            // Type of the prompt.
+        name: `userInput-${i}`,   // The name to use when storing the answer in the answers hash.
+        message: `${msg || 'Say something to start chatting!'}\n\n`, // The question to print.
+    };
 }
+
+let i = 0;
+
+function onEachAnswer({ answer }) {
+    if (answer !== '') {
+        i += 1;
+        prompts.next(makePrompt(`This is prompt #${i}.`));
+    } else {
+        prompts.complete();
+    }
+}
+
+function onError(err) {
+    console.warn(err);
+}
+
+function onCompleted() {
+    console.log('Interactive session is complete. Good bye! 🙂\n');
+}
+
+inquirer.prompt(prompts).ui.process.subscribe(
+   onEachAnswer,
+   onError,
+   onCompleted,
+);
+
+// kick off the chat with a simple call to prompts.onNext
+prompts.next(
+    makePrompt(`Hello! I will be delighted to hear about your dream 😀`)
+);

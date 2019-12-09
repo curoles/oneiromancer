7c6b73bb258351a08e0c8398f72cf03b69a0dcc2
curoles <curoles@yahoo.com>
2019-12-08 20:25:18 -0800

    split sentence into smaller chunks by conjunctions and etc

--- nlp/conversation.js
+++ nlp/conversation.js
@@ -11,10 +11,11 @@ const {split} = require('sentence-splitter');
 
 const uniqid = require('uniqid');
 
-const { NlpManager } = require('node-nlp');
-const nlp = new NlpManager();
+const { NlpManager, Language } = require('node-nlp');
+const languageNlp = new Language();
 
 //Let's load a model per Conversation instead of one for all.
+//const nlp = new NlpManager();
 //console.log("loading NLP model ...");
 //nlp.load('./build/model.nlp');
 
@@ -22,9 +23,36 @@ function createUniqueSessionId() {
     return uniqid();
 }
 
+/**
+ * Split one sentence to smaller chunks by conjunctions and etc.
+ * @param {string} lang      language of the text
+ * @param {string} sentence  one sentence
+ * @todo 1. select marks by lang, 2. parts should not include marks
+ */
+function splitSentence(lang, sentence) {
+    const punctuation = [';', ','];
+    const conjunctions = [' but ', ' and ', ' or ', ' nor '];
+    //pronouns ('either', 'neither', 'also', 'as well', 'too');
+    //adverbs ('also', 'as well', 'too');
+    const marks = [].concat(punctuation, conjunctions);
+    var chunks = [sentence];
+    for (const mark of marks) {
+        let new_chunks = [];
+        for (const chunk of chunks) {
+            let parts = chunk.split(mark);
+            new_chunks = new_chunks.concat(parts);
+        }
+        chunks = new_chunks;
+    }
+
+    return chunks;
+}
+
 function Conversation() {
     this.sessionId = createUniqueSessionId();
     this.nlp = new NlpManager();
+    this.language = undefined;
+    this.userInput = '';
 
     /** constructor */
     (() => {
@@ -34,23 +62,39 @@ function Conversation() {
     })();
 
     /**
+     * Save new chunk of text received from User.
+     * @param {string} newInput new text from User
+     * @return {void}
+     */
+    this.storeUserInput = function storeUserInput(newInput) {
+        this.userInput += newInput;
+        this.language = languageNlp.guessBest(this.userInput);
+        //console.log(this.language['alpha2']);
+    }
+
+    /**
      * Analyze input phrase and generate a response.
      * @param {string} question 
      */
     this.makeResponse = async function makeResponse(question) {
         //console.log('processing phrase: '+question);
-        let accum = '';
+        this.storeUserInput(question);
 
         let responsePromises = [];
         let sentences = split(question);
         for (let sentence of sentences) {
             if (sentence['type'] != 'Sentence') continue;
-            let partOfQuestion = sentence['raw'];
+            let sentenceOfQuestion = sentence['raw'];
 
-            responsePromises.push(this.nlp.process(partOfQuestion));
+            const partsOfSentence = splitSentence(this.language['alpha2'], sentenceOfQuestion);
+            for (const partOfSentence of partsOfSentence) {
+                responsePromises.push(this.nlp.process(partOfSentence));
+            }
         }
 
         let responses = await Promise.all(responsePromises);
+
+        let accum = '';
 
         for (let response of responses) {
             let answer = response['answer'];
